71d96cc6825e8f4fe366f15171372382bec4abc5
mungilwar <nitinmungilwar4@gmail.com>
2023-02-13 13:50:16 +0530

    completed

--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ListItems from './ListItems';
 //code here
- const List = ({listx}) =>{
-  return(
-    
- 
-    <ListItems valuex={i} />
-    
-  
-  )
-}
+const List = ({ listx }) => {
+  return (
+    <>
+      <ul id="list">
+        {listx.map((item) => (
+          <ListItems valuex={item} />
+        ))}
+      </ul>
+    </>
+  );
+};
 
 export default List;
--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React from 'react';
 
 const ListItems = (props) => {
-  return <li className="items">{props.valuex}</li>;
+  return (
+    <li className="items" key={props.valuex.id}>
+      {props.valuex}
+    </li>
+  );
 };
 export default ListItems;
